f78ae276092044f6faab33b935f14259bd90b6fc
Jean Kiepura <jkiepura@k2geospatial.com>
2021-03-18 14:49:39 -0400

    fix JWNG-244 - implement photo 360 viewer

--- public/jmap/photos.d.ts
+++ public/jmap/photos.d.ts
@@ -1,12 +1,19 @@
+declare type JPhotoProjectionType = "none" | "equirectangular"
+
 declare interface JPhoto {
   id: number
   url: string
   title: string
   fileName: string
   comment: string
+  metadata: JPhotoMetadata
   imageBase64: string
 }
 
 declare interface JPhotoEventContainerCreatedParams {
   container: HTMLElement
+}
+
+declare interface JPhotoMetadata {
+  projectionType: JPhotoProjectionType
 }
