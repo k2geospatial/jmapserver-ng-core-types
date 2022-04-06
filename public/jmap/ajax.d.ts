37142f0e37318975c743df683335fb89aea441d0
Tom <tavedissian@k2geospatial.com>
2022-04-06 09:51:45 -0400

    JWNG-585-bis - Improved JRequestConfig comments following pr comments

--- public/jmap/ajax.d.ts
+++ public/jmap/ajax.d.ts
@@ -3,24 +3,29 @@ declare interface JRequestConfig {
    * The url to request.
    */
   url: string
+
   /**
    * Body params, that will be passed in the request.
    */
   params?: { [key: string]: any } | string
+
   /**
-   * For request on Jaaz serveur only.
+   * For request on JMap servers only.
    */
   accessToken?: string
+
   /**
    * If true will return the header in the response.
    */
   includeHeadersInResponse?: boolean
+
   /**
-   * If true will ignore the authorization headers.
+   * If true will ignore the JMap authorization headers.
    */
   ignoreJMapAuthorizationHeaderParam?: boolean
+
   /**
-   * To specify custom headers that you might want to use for external ressources.
+   * To specify custom headers (Authorization, Content-type, etc) that you might want to use for external ressources.
    */
   headers?: { [key: string]: any }
 }
