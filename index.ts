118bbce47554c6bba5e2a3b9b2751310d5f13e6a
Jean Kiepura <jkiepura@k2geospatial.com>
2022-05-28 12:19:58 -0400

    [feature/JWNG-669] - get MapBox token from organization

--- index.ts
+++ index.ts
@@ -353,6 +353,7 @@ export interface JFeatureEventModule extends JEventModule {
 export interface JUserEventModule extends JEventModule {
   on: {
     sessionChanged(listenerId: string, fn: (params: JUserEventSessionChangedParams) => void): void
+    organizationChanged(listenerId: string, fn: (params: JUserEventOrganizationChangedParams) => void): void
   }
 }
 
@@ -506,6 +507,7 @@ export interface JQueryState {
 export interface JUserState {
   isLoggingIn: boolean
   identity: JUserIdentity
+  organization: JOrganization
   token: string
   informations: JUserInfo[]
   changePasswordAllowed: boolean
@@ -882,6 +884,7 @@ export interface JLayerThematicService {
 
 export interface JUserService {
   getToken(): string
+  getOrganization(): JOrganization
   getFullName(): string
   getUsername(): string
   getPreference(name: string): Promise<string | null>
--- public/jmap/user.d.ts
+++ public/jmap/user.d.ts
@@ -2,20 +2,24 @@ declare interface JUserEventSessionChangedParams {
   session: JSessionData
 }
 
+declare interface JUserEventOrganizationChangedParams {
+  organization: JOrganization
+}
+
 declare interface JSessionData {
   /**
    * The JMap user session token.
    */
   token: string
   /**
-   * The user permission to change his password 
+   * The user permission to change his password
    */
   changePasswordAllowed: boolean
   /**
    * The user information.
-   * 
+   *
    * @example ```ts
-   * 
+   *
    * user: {
    *   username: "jdo@company.com",
    *   fullName: "John Do",
@@ -24,17 +28,40 @@ declare interface JSessionData {
    * ```
    */
   user: JUserIdentity
+  /**
+   * THe JMap Cloud organization, only for JMap Cloud servers.
+   */
+  organization: JOrganization
 }
 
 declare interface JUserIdentity {
   username: string
   fullName: string
   email: string
-  organizationId: string // for jaaz only
 }
 
 declare interface JUserInfo {
   id: string
   label: string
   value: undefined | null | string | number | Date
+}
+
+declare interface JOrganization {
+  id: string
+  name: string
+  description: string
+  active: boolean
+  externalApiKeys: JExternalApiKey[]
+}
+
+declare interface JExternalApiKey {
+  id: string
+  title: string
+  apiKey: string
+  type: JEXTERNAL_API_KEY_TYPES
+}
+
+// ALL_JEXTERNAL_API_KEY_TYPES in all-enum.ts
+declare const enum JEXTERNAL_API_KEY_TYPES {
+  MAPBOX_ACCESS_TOKEN = "MAPBOX_ACCESS_TOKEN"
 }
