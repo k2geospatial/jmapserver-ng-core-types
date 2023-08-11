f040f98cee6db1c058385831392a77049d7c4054
Jean Kiepura <jkiepura@k2geospatial.com>
2023-08-11 11:06:31 -0400

    [master] - typo + whitespace corrections

--- build/gulpfile.js
+++ build/gulpfile.js
@@ -101,7 +101,7 @@ gulp.task("typedoc", cb => {
     --excludePrivate true \\
     --tsconfig ./tsconfig.json \\
     --out ${DOC_DIR} \\
-    --name "JMap Cloud NG Core types" \\
+    --name "JMap Cloud NG Core Types" \\
     --hideGenerator true \\
     --version false \\
     --disableSources true \\
